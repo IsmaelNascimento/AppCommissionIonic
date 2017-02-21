d8189446b57681fe29a556e7bf049c88b420be3c
ismaelnascimento <ismaeln.business@gmail.com>
2017-02-21 15:32:38 -0300

    GrossoTest

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $localStorage) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -8,6 +8,55 @@ angular.module('starter.controllers', [])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+
+  //var localData = $localStorage;
+  //var meuStorage = localStorage;
+  console.log("test...");
+
+  init = function(){
+    console.log("init");
+    $localStorage.test = "Data here";
+  };
+
+
+
+
+  // Save down
+  //$scope.monthlyGoal = angular.fromJson(window.localStorage['$scope.monthlyGoal'] || '0');
+  $scope.monthlyGoal = $localStorage.monthlyGoal;
+  // Save down
+  $scope.workedDays = angular.fromJson(window.localStorage['$scope.workedDays'] || '0');
+  // Save down
+  $scope.sellPerDay = angular.fromJson(window.localStorage['$scope.sellPerDay'] || '0');
+  // Save down
+  $scope.goalCurrent = angular.fromJson(window.localStorage['$scope.goalCurrent'] || '0');
+  // Save down
+  $scope.percentageCommission = angular.fromJson(window.localStorage['$scope.percentageCommission'] || '0');
+  // Save down
+  $scope.commissionWon = 0;
+
+  persistData = function(){
+    $localStorage.monthlyGoal = $scope.monthlyGoal;
+    window.localStorage['$scope.monthlyGoal'] = angular.toJson($scope.monthlyGoal);
+    window.localStorage['$scope.workedDays'] = angular.toJson($scope.workedDays);
+    window.localStorage['$scope.sellPerDay'] = angular.toJson($scope.sellPerDay);
+    window.localStorage['$scope.percentageCommission'] = angular.toJson($scope.percentageCommission);
+    window.localStorage['$scope.commissionWon'] = angular.toJson($scope.commissionWon);
+  };
+
+  $scope.SellPerDay = function(monthlyGoal, workedDays){
+    $scope.sellPerDay = monthlyGoal / workedDays;
+
+    persistData();
+  };
+
+  $scope.SumValues = function(saleValue, percentageCommission){
+    $scope.goalCurrent += saleValue;
+
+    $scope.commissionWon += (saleValue * percentageCommission) / 100;
+
+    persistData();
+  };
 
   // Form data for the login modal
   $scope.loginData = {};
@@ -39,35 +88,6 @@ angular.module('starter.controllers', [])
       $scope.closeLogin();
     }, 1000);
   };
-})
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.monthlyGoal;
-  $scope.workedDays;
-  $scope.sellPerDay = 0;
-  $scope.goal = 0;
-  $scope.percentageCommission = 0;
-  $scope.commissionWon = 0;
-
-
-
-  $scope.SellPerDay = function(monthlyGoal, workedDays){
-    $scope.sellPerDay = monthlyGoal / workedDays;
-  };
-
-  $scope.SumValues = function(saleValue, percentageCommission){
-    $scope.goal += saleValue;
-
-    $scope.commissionWon += (saleValue * percentageCommission) / 100;
-    console.log($scope.commissionWon);
-  };
-
-
-  
-  
-  
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-
+  init();
 });
